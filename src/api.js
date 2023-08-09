2ddddf9e5d46b5d6aed7ba5f0a9c16462148ffcf
worldrulesdev <worldrulesdev@gmail.com>
2023-08-09 00:24:25 -0300

    api

---
+++ src/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+
+
+const searchImages = async () => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: 'Client-ID c3b13dbb9ff78755206d5b92bd7a3c2d19bda11ebf95d51f530063e156d5fb23',
+        },
+        params: {
+            query: 'cars',
+        },
+    });
+
+    console.log(response)
+    return response;
+};
+
+export default searchImages;
